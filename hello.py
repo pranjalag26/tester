dfcafb5cc37045cab6e0cb2a4408881b94dd44ed
Jordan <90751753+linguadev@users.noreply.github.com>
2021-10-14 21:27:38 +0100

    Updated initial code.
    Replaced the simple print statement with a variable and function, created to greet the user after asking their name.

--- hello.py
+++ hello.py
@@ -1,1 +1,9 @@
-print('Hello')
+variable = "Hello"
+
+def greeting_function():
+  name = input(f"{variable}, what is your name?\n")
+  name = name.capitalize()
+  
+  print(f"{variable}, {name}. I'm pleased to meet you!\n")
+  
+greeting_function()
